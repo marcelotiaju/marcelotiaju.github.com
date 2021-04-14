70ca0aa2fddd40ecee745ad76319abbe65c7ad35
marcelotiaju <marcelotiaju@gmail.com>
2021-04-14 16:26:40 -0300

    Updates

---
+++ precache-manifest.2a50067e015fb4657adb2ab4d96836b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7b9930fe89e464a64336dab8737a89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33e6a020f9efaa4e46b3",
+    "url": "/static/js/2.c5444ead.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/static/js/2.c5444ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bdc723623b2960414b",
+    "url": "/static/js/main.f4f31df5.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
